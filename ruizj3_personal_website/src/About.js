07a93f9a2200789206f536fab9ecccb93e71635d
Joseph Ruiz <josephruiz@MacBook-Pro.domain>
2024-01-17 15:30:15 -0800

    Refactor to React App

---
+++ ruizj3_personal_website/src/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <ul>
+        <li>
+          As an accomplished BI Analyst, Engineer, and Finance professional, I bring a unique blend of technical proficiency and business acumen, 
+          honed through extensive experience in data analysis, machine learning, and software engineering. My career is marked by a commitment to 
+          leveraging powerful BI tools, Tableau dashboards, and sophisticated machine learning models to drive operational efficiency and informed 
+          decision-making. With a passion for creating data-driven products that not only address immediate needs but also generate valuable insights, 
+          I am eager to continue applying my skills in environments where innovation and data are at the forefront of business strategy.
+        </li>
+        <li>
+          My next stage is completing my PhD. (coming soon) 
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default About;
---
+++ ruizj3_personal_website/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './About'; 
+import Portfolio from './Portfolio'; 
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+<section id="hero">
+    <h1>Joseph David Ruiz</h1>
+    <p>Engineer | Finance</p>
+    <a href="Joseph Ruiz - Resume.pdf" target="_blank" rel="noopener noreferrer" class="my-link"><i class="fas fa-file-alt"></i> View My Resume</a>
+</section>
+      <Header />
+      <About />
+      <h2>Portfolio</h2>
+      <Portfolio />
+      {/* other components */}
+    </div>
+  );
+}
+
+export default App;
