79e196a6d077087d71dff35222f52e5681731233
Joseph Ruiz <ruizj3@uw.edu>
2025-11-25 14:02:23 -0800

    Update resume link path in hero section
    Changed the href in the hero section to point to 'Joseph Ruiz Resume.pdf' instead of 'Joseph Ruiz - Resume.pdf' to match the correct file name.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 <section id="hero">
     <h1>Joseph David Ruiz</h1>
     <p>Engineer | Finance</p>
-    <a href="/Joseph Ruiz - Resume.pdf" target="_blank" rel="noopener noreferrer" class="my-link"><i class="fas fa-file-alt"></i> View My Resume</a>
+    <a href="/Joseph Ruiz Resume.pdf" target="_blank" rel="noopener noreferrer" class="my-link"><i class="fas fa-file-alt"></i> View My Resume</a>
 </section>
       <Header />
       <About />
